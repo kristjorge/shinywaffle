59fed0f937705276c653967dc4032ab181b779c9
Kristian Jørgensen <kristjorge@gmail.com>
2020-04-26 11:11:07 +0200

    Refactored package structure

--- tests/historical_csv_data.py
+++ tests/historical_csv_data.py
@@ -7,26 +7,25 @@ from backtesting.account import Account
 from backtesting.brokers import Broker
 from strategy.random_signal_strategy import RandomSignalStrategy
 
-
 broker = Broker('My free broker', 0, 0, 'USD')
 random_signal_strat = RandomSignalStrategy("Random test strategy")
 
 
 # Nvidia stocks
 nvidia = Stock("Nvidia", "NVDA", "USD")
-nvidia_bars = BarProvider('/shiny-waffle/data/yahoo_finance/NVDA_1D.csv', '1d', '%Y-%m-%d')
+nvidia_bars = BarProvider('D:/PythonProjects/shiny-waffle/shiny-waffle/data/yahoo_finance/NVDA_1D.csv', '1d', '%Y-%m-%d')
 nvidia.set_bars(nvidia_bars)
 nvidia.add_strategy(random_signal_strat)
 
 # Oracle stocks
 oracle = Stock("Oracle", "ORCL", "USD")
-oracle_bars = BarProvider('/shiny-waffle/data/yahoo_finance/ORCL_1D.csv', '1d', '%Y-%m-%d')
+oracle_bars = BarProvider('D:/PythonProjects//shiny-waffle/shiny-waffle/data/yahoo_finance/ORCL_1D.csv', '1d', '%Y-%m-%d')
 oracle.set_bars(oracle_bars)
 oracle.add_strategy(random_signal_strat)
 
 # IBM stocks
 ibm = Stock("IBM", "IBM", "USD")
-ibm_bars = BarProvider('/shiny-waffle/data/yahoo_finance/IBM_1D.csv', '1d', '%Y-%m-%d')
+ibm_bars = BarProvider('D:/PythonProjects//shiny-waffle/shiny-waffle/data/yahoo_finance/IBM_1D.csv', '1d', '%Y-%m-%d')
 ibm.set_bars(ibm_bars)
 ibm.add_strategy(random_signal_strat)
 
